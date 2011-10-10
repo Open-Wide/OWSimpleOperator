3dfcadd1a2278db1bfc8b77f0682e7339ad7e61f
Thomas ZILLIOX <thomas@zilliox.me>
2011-10-10 12:11:52 +0300

    Add "public" keyword in front of the method definition.

--- autoloads/exampleoperator.php
+++ autoloads/exampleoperator.php
@@ -6,7 +6,7 @@ class ExampleOperator extends OWSimpleOperator
     /*!
      * Return the sum of two numbers
      */
-    function example_sum( $number1, $number2 )
+    public function example_sum( $number1, $number2 )
     {
         echo $number1.'+'.$number2.PHP_EOL.'<br>';
         return ( $number1 + $number2 );
