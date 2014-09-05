bcd0df905ad3bf9273bdf129bd0b85cd5757a565
Brookins Consulting <info@brookinsconsulting.com>
2014-09-05 00:40:57 -0500

    Implemented: Ini settings based custom template operator configuration

--- autoloads/eztemplateautoload.php
+++ autoloads/eztemplateautoload.php
@@ -1,13 +1,34 @@
 <?php
 
-$eZTemplateOperatorArray = array(
-     array(
-        'script' => 'extension/owsimpleoperator/autoloads/exampleoperator.php',
-        'class' => 'ExampleOperator',
-        'operator_names' => array(
-            'example_sum' ,
-        )
-     )
-);
+$classOperatorIniSettings = eZINI::instance('owsimpleoperator.ini')->variable( 'ClassOperators', 'PermittedClassOperatorList' );
+$eZTemplateOperatorArray = array();
+$classMethods = array();
+
+foreach( $classOperatorIniSettings as $settingEntryItems )
+{
+    $settingEntry = explode( ';', $settingEntryItems );
+    $classFileName = $settingEntry[0];
+    $className = $settingEntry[1];
+    $classMethod = $settingEntry[2];
+
+    if( !in_array( $className, $classMethods ) )
+    {
+        $classMethods[ $className ]['script'] = $classFileName;
+        $classMethods[ $className ]['operator_names'][] = $classMethod;
+    }
+}
+
+foreach( $classMethods as $className => $classParameters )
+{
+      $classFileName = $classParameters['script'];
+      $classMethods = $classParameters['operator_names'];
+      $eZTemplateOperatorArray[] = array( 'script' => $classFileName, 'class' => $className, 'operator_names' => $classMethods );
+}
+
+array_push( $eZTemplateOperatorArray, array(
+        'script' => 'extension/owsimpleoperator/autoloads/phpfunctionoperator.php',
+        'class' => 'PHPFunctionOperator',
+        'operator_names' => eZINI::instance('owsimpleoperator.ini')->variable( 'PHPFunctions', 'PermittedFunctionList' )
+        ) );
 
 ?>
---
+++ settings/owsimpleoperator.ini.append.php
@@ -0,0 +1,10 @@
+<?php /* #?ini charset="utf-8"?
+
+# Class file path, Class Name, Class Method
+# that are useable as template operators
+
+[ClassOperators]
+PermittedClassOperatorList[]
+PermittedClassOperatorList[]=extension/owsimpleoperator/autoloads/exampleoperator.php;ExampleOperator;example_sum
+
+*/ ?>
